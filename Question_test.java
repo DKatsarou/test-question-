be852258e0936d8d580ae9bed4576a7c6fee0c9b
DKatsarou <92544325+DKatsarou@users.noreply.github.com>
2021-11-23 19:30:06 +0200

    Update Question_test.java

--- Question_test.java
+++ Question_test.java
@@ -1,9 +1,9 @@
 import java.util.Random;
 public class Question_test {
      public static void main(String []args) {
-    	 Random rand = new Random();
-    	 int qn = rand.nextInt(63);
-    	 Questions rq = new Questions();
-    	 rq.startQuestions(qn); 
+    	 Random rand = new Random(); 
+    	 int qn = rand.nextInt(63); // random number of question is generated
+    	 Questions rq = new Questions(); // instance variable of class Questions is created
+    	 rq.startQuestions(qn); //startQuestions method is executed
      }
 }

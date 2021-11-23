ce4e6006c5eece7a1d06ed9ee99503697f12fc60
DKatsarou <92544325+DKatsarou@users.noreply.github.com>
2021-11-23 19:37:33 +0200

    Update Question_check.java

--- Question_check.java
+++ Question_check.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 public class Question_check {
 	Scanner sc = new Scanner(System.in);
 	int correctAns;
-    String Question;
-    String[] op = new String[3];
-    public Question_check(String q, String[] options, int ans, Questions quiz){
+        String Question;
+        String[] op = new String[3];
+    public Question_check(String q, String[] options, int ans, Questions quiz){  //constructor of class Question_check
         Question=q;
         correctAns = ans;
         op = options ;
     }
-    public void playq() { 
+    public void playq() { //method playq asks the user a random question 
         System.out.println("Η ερώτηση είναι: "+Question);
-        System.out.print("1. " + op[0] + "\n2. " + op[1] + "\n3. " + op[2] + "\n");
+        System.out.print("1. " + op[0] + "\n2. " + op[1] + "\n3. " + op[2] + "\n"); // the 3 options are printed on the screen 
         System.out.println("Ποια είναι η απάντηση;");
-        int a = sc.nextInt();
-        if (a == (correctAns + 1)) {
+        int a = sc.nextInt(); 
+        if (a == (correctAns + 1)) {             //depending on the answer, the user is told if he answered correctly or not
         	System.out.println("Η απάντηση είναι σωστή!");
         } else {
         	System.out.println("Λάθος! Η σωστή απάντηση είναι: " +op[correctAns]);

d0b0f1b7ac4ed03f6144ceea9834c2e539eba82b
DKatsarou <92544325+DKatsarou@users.noreply.github.com>
2021-11-23 19:28:23 +0200

    Update Question_test.java

--- Question_test.java
+++ Question_test.java
@@ -2,7 +2,7 @@ import java.util.Random;
 public class Question_test {
      public static void main(String []args) {
     	 Random rand = new Random();
-    	 int qn = rand.nextInt(20);
+    	 int qn = rand.nextInt(63);
     	 Questions rq = new Questions();
     	 rq.startQuestions(qn); 
      }
